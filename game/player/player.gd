b22310a4aa220d39bbac5f5d30a9bf426390cae2
BoneBot <cmcbride0304@gmail.com>
2025-01-20 22:26:44 -0600

    Added dash

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const ACCELERATION = SPEED * 12.0
-const JUMP_VELOCITY = -400.0
-const TERMINAL_VELOCITY = 700
+const SPEED := 300.0				# Max walking speed
+const ACCELERATION := SPEED * 12.0	# Walking acceleration
+const JUMP_VELOCITY := -400.0		# Initial jump speed
+const TERMINAL_VELOCITY := 700.0	# Max speed while falling
+const DASH_VELOCITY := 1000.0		# Initial dash speed
+
+var _dash_ready := true
 
 
 func _ready() -> void:
@@ -14,11 +17,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	# Update look direction
 	var look_threshold = 1
-	if velocity.x > look_threshold and $Sprite.flip_h:
+	if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
+		# Don't do anything when both directions are pressed at once
+		pass
+	elif Input.is_action_pressed("right") and get_look_direction() != 1:
 		# Look right
 		$Sprite.flip_h = false
 		$Sprite.offset.x = absf($Sprite.offset.x)
-	elif velocity.x < -look_threshold and not $Sprite.flip_h:
+	elif Input.is_action_pressed("left") and get_look_direction() != -1:
 		# Look left
 		$Sprite.flip_h = true
 		$Sprite.offset.x = -absf($Sprite.offset.x)
@@ -39,9 +45,35 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right") * SPEED
 	velocity.x = move_toward(velocity.x, direction, ACCELERATION * delta)
 
+	# Handle dash
+	if Input.is_action_just_pressed("dash") and _dash_ready:
+		_dash_ready = false
+		$DashCooldown.start()
+		var movement_threshold = 0.1
+		if direction > movement_threshold:
+			# Dash right
+			velocity.x = DASH_VELOCITY
+		elif direction < -movement_threshold:
+			# Dash left
+			velocity.x = -DASH_VELOCITY
+		else:
+			# No current movement; base dash direction on the direction the character is looking
+			velocity.x = DASH_VELOCITY * get_look_direction()
+
 	move_and_slide()
 
 
-func flip_sprite() -> void:
-	$Sprite.flip_h = !$Sprite.flip_h
-	$Sprite.offset.x = -$Sprite.offset.x
+# Gets the current direction the player is looking
+# 1 = Looking right 
+# 0 = Neutral/no direction
+# -1 = Looking left 
+func get_look_direction() -> int:
+	if not $Sprite.flip_h:
+		return 1
+	else:
+		return -1
+
+
+func _on_dash_cooldown_timeout() -> void:
+	print("Dash ready!")
+	_dash_ready = true
