0292d05aa6b4db08f5789782d870eb612c9f36ae
BoneBot <cmcbride0304@gmail.com>
2025-01-20 21:28:36 -0600

    Added temp sprite, updated controls, and improved jumping

--- game/player/player.gd
+++ game/player/player.gd
@@ -3,16 +3,24 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const TERMINAL_VELOCITY = 700
+
+
+func _ready() -> void:
+	pass
+
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = 0
+
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y = minf(TERMINAL_VELOCITY, velocity.y + get_gravity().y * delta)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +31,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func flip_sprite() -> void:
+	$Sprite.flip_h = !$Sprite.flip_h
+	$Sprite.offset.x = -$Sprite.offset.x
