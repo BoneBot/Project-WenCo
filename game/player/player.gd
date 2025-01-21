6a2d8d8f66b8f5a12f92ceba76abae6fa070028c
BoneBot <cmcbride0304@gmail.com>
2025-01-20 21:46:02 -0600

    Flipped player sprite and tweaked movement

--- game/player/player.gd
+++ game/player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const ACCELERATION = SPEED * 12.0
 const JUMP_VELOCITY = -400.0
 const TERMINAL_VELOCITY = 700
 
@@ -10,25 +11,33 @@ func _ready() -> void:
 	pass
 
 
+func _process(delta: float) -> void:
+	# Update look direction
+	var look_threshold = 1
+	if velocity.x > look_threshold and $Sprite.flip_h:
+		# Look right
+		$Sprite.flip_h = false
+		$Sprite.offset.x = absf($Sprite.offset.x)
+	elif velocity.x < -look_threshold and not $Sprite.flip_h:
+		# Look left
+		$Sprite.flip_h = true
+		$Sprite.offset.x = -absf($Sprite.offset.x)
+
 
 func _physics_process(delta: float) -> void:
 	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	elif Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y = 0
+		velocity.y *= 0.2
 
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y = minf(TERMINAL_VELOCITY, velocity.y + get_gravity().y * delta)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Handle horizontal movement
+	var direction := Input.get_axis("left", "right") * SPEED
+	velocity.x = move_toward(velocity.x, direction, ACCELERATION * delta)
 
 	move_and_slide()
 
