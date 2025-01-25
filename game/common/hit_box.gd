51c0ae04a7eeed3c693048990a0973bbfc669c7b
BoneBot <cmcbride0304@gmail.com>
2025-01-25 16:57:16 -0600

    Knight now flips properly and takes damage

--- game/common/hit_box.gd
+++ game/common/hit_box.gd
@@ -10,7 +10,5 @@ func _ready() -> void:
 
 
 func _on_area_entered(area: Area2D) -> void:
-	print("Hit something...")
 	if area.has_method("take_damage"):
-		print("Something can take damage!")
 		area.take_damage(damage)
--- game/enemies/knight.gd
+++ game/enemies/knight.gd
@@ -11,11 +11,11 @@ extends RigidBody2D
 
 func _ready() -> void:
 	if not facing_right:
+		facing_right = true	# Temporarily setting to true so the sprite will turn left when it flips
 		turn_around()
 
 
 func turn_around() -> void:
-	print("Flip!")
 	if facing_right:
 		# Turn to face left
 		sprite.flip_h = true
@@ -34,12 +34,17 @@ func _on_vision_body_entered(body: Node2D) -> void:
 	attack_timer.start()
 
 
-func _on_vision_body_exited(body: Node2D) -> void:
-	attack_timer.stop()
-
-
 func _on_attack_timer_timeout() -> void:
+	# Attack
 	if facing_right:
 		animation_player.play("attack_right")
 	else:
 		animation_player.play("attack_left")
+
+	# Stop attacking if player is no longer in vision range
+	if not vision.has_overlapping_bodies():
+		attack_timer.stop()
+
+
+func _on_hurt_box_damage_taken(damage: Variant) -> void:
+	print("Knight took %d damage" % damage)
