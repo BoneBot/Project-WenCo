b18d57e1f44af39c4f271a73749c031c3176d6de
BoneBot <cmcbride0304@gmail.com>
2025-01-24 19:05:44 -0600

    Player fires sword

--- game/player/player.gd
+++ game/player/player.gd
@@ -1,13 +1,32 @@
 extends CharacterBody2D
 
 
+# CHILDREN
+@onready var sprite := $Sprite
+@onready var animation_player := $AnimationPlayer
+@onready var dash_cooldown := $DashCooldown
+@onready var projectile_spawn := $ProjectileSpawn
+
+# CONSTANTS
 const SPEED := 300.0				# Max walking speed
 const ACCELERATION := SPEED * 12.0	# Walking acceleration
 const JUMP_VELOCITY := -400.0		# Initial jump speed
 const TERMINAL_VELOCITY := 700.0	# Max speed while falling
 const DASH_VELOCITY := 1000.0		# Initial dash speed
 
-var _dash_ready := true
+# EXPORT VARIABLES
+@export var sword_scene: PackedScene
+
+# MEMBER VARIABLES
+var dash_ready := true
+var sword_instance: RigidBody2D
+
+# ENUMS
+enum LookDirectionType {
+	LEFT = -1,
+	NEUTRAL = 0,
+	RIGHT = 1
+}
 
 
 func _ready() -> void:
@@ -19,19 +38,17 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
 		# Don't do anything when both directions are pressed at once
 		pass
-	elif Input.is_action_pressed("right") and get_look_direction() != 1:
+	elif Input.is_action_pressed("right") and get_look_direction() != LookDirectionType.RIGHT:
 		# Look right
-		$Sprite.flip_h = false
-		$Sprite.offset.x = -absf($Sprite.offset.x)
-	elif Input.is_action_pressed("left") and get_look_direction() != -1:
+		look_at_direction(LookDirectionType.RIGHT)
+	elif Input.is_action_pressed("left") and get_look_direction() != LookDirectionType.LEFT:
 		# Look left
-		$Sprite.flip_h = true
-		$Sprite.offset.x = absf($Sprite.offset.x)
+		look_at_direction(LookDirectionType.LEFT)
 	
 	# Update animation
 	var animation := get_new_animation(Input.is_action_just_pressed("attack"))
-	if animation != $AnimationPlayer.current_animation:
-		$AnimationPlayer.play(animation)
+	if animation != animation_player.current_animation:
+		animation_player.play(animation)
 
 
 func _physics_process(delta: float) -> void:
@@ -50,9 +67,9 @@ func _physics_process(delta: float) -> void:
 	velocity.x = move_toward(velocity.x, direction, ACCELERATION * delta)
 
 	# Handle dash
-	if Input.is_action_just_pressed("dash") and _dash_ready:
-		_dash_ready = false
-		$DashCooldown.start()
+	if Input.is_action_just_pressed("dash") and dash_ready:
+		dash_ready = false
+		dash_cooldown.start()
 		var movement_threshold = 0.1
 		if direction > movement_threshold:
 			# Dash right
@@ -64,29 +81,44 @@ func _physics_process(delta: float) -> void:
 			# No current movement; base dash direction on the direction the character is looking
 			velocity.x = DASH_VELOCITY * get_look_direction()
 
+	# Handle blink
+	if Input.is_action_just_pressed("blink"):
+		sword_instance = sword_scene.instantiate()
+		sword_instance.position = projectile_spawn.position
+		add_child(sword_instance)
+		sword_instance.apply_central_impulse(Vector2(400, -400))
+
 	move_and_slide()
 
 
 func _on_dash_cooldown_timeout() -> void:
-	_dash_ready = true
+	dash_ready = true
 
 
 # Gets the current direction the player is looking
-# 1 = Looking right 
-# 0 = Neutral/no direction
-# -1 = Looking left 
-func get_look_direction() -> int:
-	if not $Sprite.flip_h:
-		return 1
+func get_look_direction() -> LookDirectionType:
+	if not sprite.flip_h:
+		return LookDirectionType.RIGHT
 	else:
-		return -1
+		return LookDirectionType.LEFT
+
+
+func look_at_direction(direction: LookDirectionType) -> void:
+	if direction == LookDirectionType.RIGHT:
+		# Look right
+		sprite.flip_h = false
+		sprite.offset.x = -absf(sprite.offset.x)
+	elif direction == LookDirectionType.LEFT:
+		# Look left
+		sprite.flip_h = true
+		sprite.offset.x = absf(sprite.offset.x)
 
 
 # Gets the current animation to play based on the player state
 func get_new_animation(isAttacking: bool) -> String:
 	var animation_new: String
 	if is_on_floor():
-		if isAttacking or ($AnimationPlayer.current_animation == "attack"):
+		if isAttacking or (animation_player.current_animation == "attack"):
 			animation_new = "attack"
 		elif absf(velocity.x) > 0.1:
 			animation_new = "run"
--- game/player/sword.gd
+++ game/player/sword.gd
@@ -1,28 +1,17 @@
 extends RigidBody2D
 
 
-# The initial launch velocity of the sword
-const LAUNCH_VELOCITY := Vector2(300, 300)
-const TERMINAL_VELOCITY := 700.0
-
-var allow_rotation := true
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _physics_process(delta: float) -> void:
-	if get_contact_count() == 0 and allow_rotation:
-		# Follow the current trajectory if in the air and allowed to rotate
-		look_at(linear_velocity)
-	elif allow_rotation:
-		# Stop allowing to rotate once an object is hit
-		allow_rotation = false
+	if linear_velocity.length() > 0.1:
+		# Follow the current trajectory while moving
+		look_at(linear_velocity + position)
 
-	if Input.is_action_just_pressed("blink"):
-		var impulse = Vector2(100, -800)
-		print("Wheeee!")
-		apply_central_impulse(impulse)
-		allow_rotation = true
+
+func _on_body_entered(body: Node) -> void:
+	# Destroy sword when it comes into contact with a body
+	queue_free()
