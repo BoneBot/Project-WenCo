0647957ab9a58b8bf20e10cfb49af76c5ee2c9a8
BoneBot <cmcbride0304@gmail.com>
2025-01-30 22:10:41 -0600

    Player animations done

--- game/player/player.gd
+++ game/player/player.gd
@@ -29,9 +29,11 @@ const BLINK_IMPULSE_MAGNITUE := 750		# Impulse applied when throwing weapon for 
 
 # MEMBER VARIABLES
 var sword_instance: RigidBody2D
+var movement_enabled := true
+var dash_enabled := true
+var blink_enabled := true
 var dash_ready := true
 var blink_ready := true
-var is_vanished := false
 
 # ENUMS
 enum LookDirectionType {
@@ -67,17 +69,17 @@ func _process(delta: float) -> void:
 
 ## Process player physics
 func _physics_process(delta: float) -> void:
-	# Do not process any player physics if they are vanished
-	if is_vanished:
+	# Do not process any player physics if movement is not enabled
+	if not movement_enabled:
 		return
 
-	if Input.is_action_just_released("blink") and blink_ready:
+	if Input.is_action_just_released("blink") and blink_enabled and blink_ready:
 		blink_ready = false
 		if Input.get_axis("left", "right") or Input.get_axis("up", "down"):
 			trigger_blink()
 		else:
 			blink_cooldown.start()
-	elif Input.is_action_pressed("blink") and blink_ready:
+	elif Input.is_action_pressed("blink") and blink_enabled and blink_ready:
 		# Handle blink-hold inputs
 		handle_blink_hold_inputs()
 	else:
@@ -105,8 +107,8 @@ func handle_normal_inputs(delta: float) -> void:
 	velocity.x = move_toward(velocity.x, direction, ACCELERATION * delta)
 
 	# Handle dash
-	if Input.is_action_just_pressed("dash") and dash_ready:
-		# Disable dash
+	if Input.is_action_just_pressed("dash") and dash_enabled and dash_ready:
+		# Start dash cooldown
 		dash_ready = false
 		dash_cooldown.start()
 
@@ -124,6 +126,9 @@ func handle_normal_inputs(delta: float) -> void:
 		# Also reset vertical velocity
 		velocity.y = 0
 
+		# Play animation
+		animation_player.play("dash")
+
 
 ## Handles inputs when the blink button is held down
 func handle_blink_hold_inputs() -> void:
@@ -134,11 +139,34 @@ func handle_blink_hold_inputs() -> void:
 
 ## Initiates a blink
 func trigger_blink() -> void:
+	# Freeze player
+	movement_enabled = false
+	camera.enabled = false
+	collision.set_deferred("disabled", true)
+
+	# Create and add sword
 	create_sword_instance()
-	vanish_player()
 	add_child(sword_instance)
 	var direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
 	sword_instance.apply_central_impulse(direction * BLINK_IMPULSE_MAGNITUE)
+
+	# Trigger blink start animation
+	animation_player.play("blink_start")
+
+
+## Terminates a blink
+func _on_blink_triggered(blink_position) -> void:
+	# Unfreeze player
+	position = blink_position
+	camera.enabled = true
+	sprite.visible = true
+	collision.set_deferred("disabled", false)
+
+	# Start blink cooldown
+	blink_cooldown.start()
+
+	# Trigger blink end animation
+	animation_player.play("blink_end")
 
 
 ## Gets the current direction the player is looking
@@ -162,20 +190,30 @@ func look_at_direction(direction: LookDirectionType) -> void:
 ## Gets the current animation to play based on the player state
 func get_new_animation(isAttacking: bool) -> String:
 	var animation_new: String
+	var is_moving_threshold = 0.1
+
+	# Do not interrupt attacking, blinking, or taking damage
+	if animation_player.current_animation in ["attack", "blink_start", "blink_end", "hurt"] and animation_player.is_playing():
+		return animation_player.current_animation
+
+	# Do not interrupt dash if moving
+	if animation_player.current_animation == "dash" and absf(velocity.x) > is_moving_threshold and animation_player.is_playing():
+		return animation_player.current_animation
+
+	# Determine next animation
 	if is_on_floor():
-		if "attack" in animation_player.current_animation and animation_player.is_playing():
-			animation_new = animation_player.current_animation
-		elif isAttacking:
+		if isAttacking:
 			animation_new = "attack"
 		elif absf(velocity.x) > 0.1:
 			animation_new = "run"
 		else:
 			animation_new = "idle"
 	else:
-		if animation_player.current_animation not in ["fall", "attack"] and animation_player.is_playing():
+		if animation_player.current_animation != "fall" and animation_player.is_playing():
 			animation_new = "jump"
 		else:
 			animation_new = "fall"
+
 	return animation_new
 
 
@@ -185,37 +223,15 @@ func create_sword_instance() -> void:
 	sword_instance.initialize(projectile_spawn.position, _on_blink_triggered)
 
 
-## Vanishes the player to begin a blink
-func vanish_player() -> void:
-	camera.enabled = false
-	sprite.visible = false
-	collision.set_deferred("disabled", true)
-	is_vanished = true
-
-
-## Unvanishes the player to terminate a blink
-func unvanish_player() -> void:
-	camera.enabled = true
-	sprite.visible = true
-	collision.set_deferred("disabled", false)
-	is_vanished = false
-
-
 ## Kills the player
 func die() -> void:
 	sprite.visible = false
 	collision.set_deferred("disabled", true)
-	is_vanished = true
+	movement_enabled = false
 
 
 func _on_dash_cooldown_timeout() -> void:
 	dash_ready = true
-
-
-func _on_blink_triggered(blink_position) -> void:
-	position = blink_position
-	unvanish_player()
-	blink_cooldown.start()
 
 
 func _on_blink_cooldown_timeout() -> void:
@@ -225,7 +241,15 @@ func _on_blink_cooldown_timeout() -> void:
 
 func _on_hurt_box_damage_taken(damage: Variant) -> void:
 	print("Player took %d damage!" % damage)
+	animation_player.play("hurt")
 	health = health - damage
 	if health <= 0:
 		print("Player died!")
 		die()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "blink_start":
+		sprite.visible = false
+	elif anim_name == "blink_end":
+		movement_enabled = true
